c224351a3fa6486a8017b7dcb352ee07dd0f99a3
Air Haji Satellite Tracking <90659788+yc5abk@users.noreply.github.com>
2021-09-29 10:08:46 +0700

    Add DB Migration for AT Job ID

--- db_migrations/update_database.sh
+++ db_migrations/update_database.sh
@@ -10,7 +10,7 @@
 #       can map each script to a check for whether an item exists already, then
 #       loop over that struct.
 
-. "$HOME/.noaa-v2.conf"
+. "$HOME/.stb_Noaa.conf"
 . "$NOAA_HOME/scripts/common.sh"
 
 SQL_CMD=/usr/bin/sqlite3
